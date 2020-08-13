8009ae8e166bb877436b478cf55c54dc720cd8cb
Lucas Melo <lucasmelo@protonmail.com>
2020-08-13 10:36:34 -0300

    fix knex client db

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -2,12 +2,12 @@ import knex from 'knex';
 import 'dotenv/config';
 
 const db = knex({
-  client: 'postgres',
+  client: 'pg',
   connection: {
-    host : process.env.PG_HOST,
-    user : process.env.PG_USER,
-    password : process.env.PG_PASSWORD,
-    database : process.env.PG_DATABASE
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
   },
   useNullAsDefault: true
 });
